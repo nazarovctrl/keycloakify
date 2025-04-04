f22f8217fcc58621d41529314d00fda0dffe9970
nazarovctrl <nazarovctrl@gmail.com>
2025-04-04 09:44:36 +0500

    Uzbek language added

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,15 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withExtraLanguages({
+        uz: {
+            label: "Uzbek",
+            getMessages: () => import("./i18n.uz")
+        }
+    })
+    .build();
 
 type I18n = typeof ofTypeI18n;
 
